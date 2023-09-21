76bddf2d6b9ec1e56238c4d878f8761ed35688a5
Cesar Vega <g.vega.cl@gmail.com>
2023-09-21 15:19:59 -0400

    add function declaration to components and add IParallaxMotionBoxProps

--- frontend/app/components/atoms/MotionBox.tsx
+++ frontend/app/components/atoms/MotionBox.tsx
@@ -1,9 +1,14 @@
 import { Box, ButtonProps } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 
+// Open for extension & shuts up eslint. This is an ESLint bug.
+// because doing { children }: ButtonProps makes ESLint say that 
+// ButtonProps is not used
+interface IParallaxMotionBoxProps extends ButtonProps {}
+
 export const MotionBox = motion(Box);
 
-export const ParallaxMotionBox = ({ children }: ButtonProps) => {
+export const ParallaxMotionBox = ({ children }: IParallaxMotionBoxProps) => {
   return (
     <MotionBox
       initial={{ opacity: 0, y: -10 }}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,15 +2,19 @@
 import { Flex } from "@chakra-ui/react";
 import { ButtonSize, PinkButton } from "@/app/components/atoms";
 
-export default function Home() {
+const Home = () => {
   return (
     <main>
       <Flex className="gap-8">
         <PinkButton buttonSize={ButtonSize.MD} href="/solid">
           SOLID
         </PinkButton>
-        <PinkButton buttonSize={ButtonSize.MD} href="/code_smells">Code Smells</PinkButton>
+        <PinkButton buttonSize={ButtonSize.MD} href="/code_smells">
+          Code Smells
+        </PinkButton>
       </Flex>
     </main>
   );
-}
+};
+
+export default Home;
