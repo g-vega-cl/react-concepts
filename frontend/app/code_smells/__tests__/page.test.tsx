921585126e84a722be0f176717a410a8dbdf1ed7
Cesar Vega <g.vega.cl@gmail.com>
2023-09-23 15:48:51 -0400

    fix lint errors

--- frontend/app/code_smells/__tests__/page.test.tsx
+++ frontend/app/code_smells/__tests__/page.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import Page from "../page";  // Replace with the correct path to your component
+import Page from "../page";
 
 describe("<Page />", () => {
   it("renders without crashing", () => {
